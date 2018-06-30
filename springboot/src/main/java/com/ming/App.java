b6754cf6c8dac16a0ffbe2816f4b5e7a5311fb76
ming_he <xiaoming_he@outlook.com>
2018-06-30 18:05:17 +0800

    spring boot使用mybatis-start和druid-start

--- springboot/src/main/java/com/ming/App.java
+++ springboot/src/main/java/com/ming/App.java
@@ -2,12 +2,13 @@ package com.ming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author ming_he
  * @date 2018/6/23 21:29
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class App {
 
     public static void main(String[] args) {
---
+++ springboot/src/main/java/com/ming/domain/User.java
@@ -0,0 +1,19 @@
+package com.ming.domain;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author ming_he
+ * @date 2018/6/27 23:38
+ */
+@Data
+public class User {
+
+    private long id;
+    private String userName;
+    private String realName;
+    private int age;
+    private BigDecimal balance;
+}
