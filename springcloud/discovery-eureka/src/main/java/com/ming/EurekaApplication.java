0ec24073bb4775779eb1eb021a4f745860a9ae6e
ming_he <ming_he@kingdee.com>
2018-09-01 17:26:42 +0800

    添加apollo配置中心

--- springcloud/discovery-eureka/src/main/java/com/ming/EurekaApplication.java
+++ springcloud/discovery-eureka/src/main/java/com/ming/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.ming;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableApolloConfig("001.eureka-server")
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
